483189fe7a2d23e26123592be676efd4a9a46d61
Pedro <elprogramadorpedro@gmail.com>
2021-10-14 17:21:38 -0300

    Refactoring the planet component using hook use state and hook use effect

--- spaceships/src/component/planets/planet/index.js
+++ spaceships/src/component/planets/planet/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef */
 
-import React from "react";
+import React,{useEffect, useState} from "react";
 import DescriptionWithLink from "../../descriptionWithLink";
 import GrayImg from "../../shared/gray_img";
 
@@ -11,8 +12,17 @@ async function getSatellites(id){
 
 
 
-class Planet extends React.Component{
+const Planet =(props)=>{
   
+  const [satellites, setSatellites] = useState([
+  ])
+
+  useEffect(()=>{ 
+    getSatellites(props.id).then(data=>{
+      setSatellites(data['satellites'])
+    })
+    }, [])
+  /*
   constructor(props){
     super(props)
     this.state={
@@ -26,33 +36,29 @@ class Planet extends React.Component{
         satellites:data['satellites']
       }))
     })
-  }
+  }*/
   
 
-  render(){
-
-    
-
-
   let title;
-  if(this.props.title_with_underline)
-    title=<h4><u>{this.props.name}</u></h4>
+  if(props.title_with_underline)
+    title=<h4><u>{props.name}</u></h4>
   else
-  title=<h4>{this.props.name}</h4>
+  title=<h4>{props.name}</h4>
     return (
       <div>
        {title}
-        <DescriptionWithLink description={this.props.description} link={this.props.link}/>
-        <GrayImg img_url={this.props.img_url} gray={this.props.gray}/>
+        <DescriptionWithLink description={props.description} link={props.link}/>
+        <GrayImg img_url={props.img_url} gray={props.gray}/>
+        
   <ol>
-    {this.state.satellites.map((satellite, index)=>
+    {satellites.map((satellite, index)=>
       <li key={index}>{satellite.name}</li>
     )}
 
     </ol>
       </div>
     );
-}
+
 }
 
 

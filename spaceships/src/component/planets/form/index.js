aec27e72c34c7dca881c9450f81e122378c1cab6
Pedro <elprogramadorpedro@gmail.com>
2021-10-15 14:02:32 -0300

    form with multiple controlled impust

--- spaceships/src/component/planets/form/index.js
+++ spaceships/src/component/planets/form/index.js
@@ -1,17 +1,31 @@
 import React, {useState, Fragment } from 'react'
 
+//para limpar camps
+
+const initialState ={
+    name:'',
+    description:'',
+    link:'',
+    img_url:''
+}
+
 
 
 
 const Form = (props) =>{
-const [name, setName]=useState('')
-const handleChange=(event)=>setName(event.target.value);
+const [fields, setFieds]=useState(initialState) //un estads para todos los campos
+
+const handleFieldsChange=(event)=>setFieds({
+...fields,
+[event.currentTarget.name]: event.currentTarget.value
+});
 
 
 //handleSubmit para adicionar planeta metodo para adicionar planeta
 const handleSubmit=event=>{
-    props.addPlanet({name:name})
+    props.addPlanet(fields)
     event.preventDefault();
+    setFieds(initialState);
 }
 
 
@@ -20,7 +34,19 @@ return(
         <form onSubmit={handleSubmit}>
         <div>
         <label htmlForm="name">Name:</label>
-        <input id="name" type="text" value={name} onChange={handleChange}/>
+        <input id="name" type="text" name="name" value={fields.name}  onChange={handleFieldsChange}/>
+        </div>
+        <div>
+        <label htmlForm="name">Description:</label>
+        <input id="description" type="text" name="description" value={fields.description}  onChange={handleFieldsChange}/>
+        </div>
+        <div>
+        <label htmlForm="name">Img_url:</label>
+        <input id="img_url" type="text" name="img_url" value={fields.img_url}  onChange={handleFieldsChange}/>
+        </div>
+        <div>
+        <label htmlForm="name">link:</label>
+        <input id="link" type="text" name="link" value={fields.link}  onChange={handleFieldsChange}/>
         </div>
         <br/>
         <input type="submit"/>
